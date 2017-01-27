a17cdf249b6c8cda086703c96b808fd36b47ca61
Shawn Shaligram <sshaligram@lyft.com>
2017-01-26 21:28:20 -0800

    setup the scraper

---
+++ scraper.py
@@ -0,0 +1,33 @@
+# Gets current stock price data from google finance
+# for all stocks in the ticker.txt file
+# Author: Shawn Shaligram
+
+import urllib
+from bs4 import BeautifulSoup
+
+tickerFile = open("ticker.txt")
+tickerList = tickerFile.read()
+tickerArr = tickerList.split("\n")
+BASE_URL = "https://www.google.com/finance?q=NASDAQ%3A"
+
+def get_stock_price():
+    """
+    Scrapes google finance and the ticket symbol
+    page and returns stock price for each symbol
+    """
+    try:
+        print "Attempt to gather price data for ticker symbols"
+        for ticker in tickerArr:
+            htmlfile = urllib.urlopen(BASE_URL + ticker)
+            htmltext = htmlfile.read()
+
+            soup = BeautifulSoup(htmltext, 'html.parser')
+            market_data = soup.find('span', attrs={'class' : 'pr'})
+            stock_price = market_data.text.strip()
+
+            print ticker,": $",stock_price
+    except Exception, e:
+        print str(e)
+        print 'Error occured in returning stock price. Please ensure the ticker symbol is correct'
+
+get_stock_price()
